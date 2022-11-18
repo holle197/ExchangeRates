cc27fde53d80201bbaa5736f20f3cc10399b72e9
99999marko@gmail.com <99999marko@gmail.com>
2022-11-18 20:11:01 +0100

    Test Project added

---
+++ ExchangeRates.Test/CoreTesting/FetchersTesting/TestingFixerFetcher.cs
@@ -0,0 +1,26 @@
+﻿using ExchangeRates.Core.Fetchers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExchangeRates.Test.CoreTesting.FetchersTesting
+{
+    public class TestingFixerFetcher
+    {
+        [Fact]
+
+        public async Task FetchSymbols_OnSuccess_ReturnsISymbols()
+        {
+            var apikey = "nQ79FQEm879L7xHxyPORbMD6PPofZMvk";
+            var fetcher = new FixerFetcher(apikey);
+
+            var res = await fetcher.FetchAllSymbolsAsync();
+
+            Assert.NotNull(res);
+            Assert.True(res?.Count > 0);
+        }
+    }
+}
